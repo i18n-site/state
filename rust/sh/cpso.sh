994bdec85477f22191c3eae3bb3d7269022897ec
i18n <i18n.site@gmail.com>
2025-02-17 21:40:10 +0800

    ♨

--- rust/sh/cpso.sh
+++ rust/sh/cpso.sh
@@ -7,5 +7,7 @@ set -ex
 target=$(rustc -vV | grep "host:" | awk '{print $2}')
 
 mkdir -p /so
-# cp /usr/lib/libgcc_s.so.* /so
+
 ldd $1 | grep "=> /" | awk '{print $3}' | xargs -I '{}' sh -c 'cp -L "{}" /so/'
+
+# cp /usr/lib/libgcc_s.so.* /so
