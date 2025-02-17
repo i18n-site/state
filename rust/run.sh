03db13555ee7b9cdd17aabc99a8c02c747faaa33
i18n <i18n.site@gmail.com>
2025-02-17 23:10:28 +0800

    ♨

--- rust/run.sh
+++ rust/run.sh
@@ -2,18 +2,15 @@
 
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
-set -ex
 
 set -e
 . .project.sh
-set -x
 
 set +a
-
 for f in ../conf/rust/*.env; do
   . $f
 done
-
 set -a
 
+set -x
 exec ./.run.sh $project
